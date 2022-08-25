decbe889e95f5396d6b28e3811847b0ca8d01b73
jlclosada <jlcaclosada@gmail.com>
2022-08-25 21:15:06 +0200

    Font Change

--- youtube.py
+++ youtube.py
@@ -7,12 +7,11 @@ def download():
     link = videos.get()
     video = YouTube(link)
     download = video.streams.get_highest_resolution()
-    download.download(output_path = "D:\Descargas")
-    
+    download.download(output_path="D:\Descargas")
+
+
 def popup():
     MessageBox.showinfo("About me", "Software Engineer, Spain.")
-
-
 
 
 root = Tk()
@@ -29,18 +28,21 @@ helpmenu = Menu(menubar, tearoff=0)
 
 
 menubar.add_cascade(label="Sobre este programa", menu=helpmenu)
-helpmenu.add_cascade(label = "Información del autor", command = popup)
+helpmenu.add_cascade(label="Información del autor", command=popup)
 
-menubar.add_cascade(label = "Salir", command = root.destroy)
+menubar.add_cascade(label="Salir", command=root.destroy)
 
-instructions = Label(root, text = "Bienvenido al YouTube downloader de Joss")
-instructions.config(font = ("Courier", 23, "bold"))
-instructions.grid(row =0, column = 1)
+instructions = Label(root, text="Bienvenido al YouTube downloader de Joss")
+instructions.config(font=("Courier", 23, "bold"))
+instructions.grid(row=0, column=1)
 
-videos = Entry(root, width = 90)
-videos.grid(row = 1, column = 1)
+videos = Entry(root, width=90)
+videos.grid(row=1, column=1)
 
-boton = Button(root, text = "Download", command = download)
-boton.grid(row = 2, column = 1)
+boton = Button(root, text="Download", command=download)
+boton.grid(row=2, column=1)
 
 root.mainloop()
+
+
+label = "Hola"
