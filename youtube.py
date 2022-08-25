8654ece36dad913ede7f5e722f4abfb1cc3b3da8
jlclosada <jlcaclosada@gmail.com>
2022-08-25 23:09:11 +0200

    Edit download function

--- youtube.py
+++ youtube.py
@@ -1,13 +1,24 @@
 from pytube import YouTube
 from tkinter import *
 from tkinter import messagebox as MessageBox
+from tkinter import filedialog
+
+
+def get_path():
+    path = filedialog.askdirectory()
+    MessageBox.showinfo("Selección de carpeta",
+                        "Se ha seleccionado la carpeta de descarga correctamente!")
+    return path
 
 
 def download():
     link = videos.get()
     video = YouTube(link)
     download = video.streams.get_highest_resolution()
-    download.download(output_path="D:\Descargas")
+    path = filedialog.askdirectory()
+    MessageBox.showinfo("Selección de carpeta",
+                        "Se ha seleccionado la carpeta de descarga correctamente!")
+    download.download(output_path=path)
 
 
 def popup():
@@ -33,13 +44,18 @@ helpmenu.add_cascade(label="Información del autor", command=popup)
 menubar.add_cascade(label="Salir", command=root.destroy)
 
 instructions = Label(root, text="Bienvenido al YouTube downloader de Joss")
-instructions.config(font=("Courier", 23, "bold"))
+instructions.config(font=("Cascadia Code", 23, "bold"))
 instructions.grid(row=0, column=1)
 
+instructions2 = Label(
+    root, text="Introduzca el enlace del video a descargar:")
+instructions2.config(font=("Cascadia Code", 15, "italic"))
+instructions2.grid(row=1, column=1)
+
 videos = Entry(root, width=90)
-videos.grid(row=1, column=1)
+videos.grid(row=2, column=1)
 
 boton = Button(root, text="Download", command=download)
-boton.grid(row=2, column=1)
+boton.grid(row=3, column=1)
 
 root.mainloop()
